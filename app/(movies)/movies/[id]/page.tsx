0a87fb21a1f63ae65ec7c67768fdbda880d955af
dadak797 <dadak797@gmail.com>
2024-02-26 13:38:46 +0900

    First commit

---
+++ app/(movies)/movies/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { Suspense } from "react";
+import MovieInfo from "../../../components/movie-info";
+import MovieVideos from "../../../components/movie-videos";
+
+export default async function MovieDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  return (
+    <h1>
+      <h3>Movie Detail Page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie vidoes</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </h1>
+  );
+}
