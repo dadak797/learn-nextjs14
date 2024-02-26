93b6e01f61b72b41ae604d7f2cd7a68da9ab7a79
dadak797 <dadak797@gmail.com>
2024-02-26 16:06:47 +0900

    4.3

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -8,14 +8,13 @@ export default async function MovieDetail({
   params: { id: string };
 }) {
   return (
-    <h1>
-      <h3>Movie Detail Page</h3>
+    <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
       <Suspense fallback={<h1>Loading movie vidoes</h1>}>
         <MovieVideos id={id} />
       </Suspense>
-    </h1>
+    </div>
   );
 }
